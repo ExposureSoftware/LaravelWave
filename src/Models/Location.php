0182254c5408f7e5f3d0ba6ed8a72e8cfbb0eea3
Marshall Davis <toothlessrebel@gmail.com>
2020-03-17 10:02:32 -0400

    Remove unhelpful annotations.

--- src/Models/Location.php
+++ src/Models/Location.php
@@ -6,12 +6,6 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
-/**
- * Class Location
- *
- * @package ExposureSoftware\LaravelWave\Models
- * @method Builder hasDevices(Builder $query)
- */
 class Location extends Model
 {
     public $incrementing = false;
